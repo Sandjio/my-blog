01a7cdef08b650410f9c6a76998f933f9c4b259e
sandjio <emmasandjio@gmail.com>
2024-12-21 00:22:47 +0100

    Extend the base.html to post_list.html and post_detail.html

--- Posts/urls.py
+++ Posts/urls.py
@@ -5,5 +5,6 @@ from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    # path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("post/", PostDetailView.as_view(), name="post_detail"),
 ]
--- Posts/views.py
+++ Posts/views.py
@@ -1,19 +1,27 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import Post
 
 
 # Create your views here.
-class PostListView(ListView):
-    model = Post
-    template_name = "posts/post_list.html"
-    context_object_name = "posts"
-    ordering = ["-created_at"]
-    paginate_by = 5
+class PostListView(TemplateView):
+    template_name = "post_list.html"
 
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = "posts/post_detail.html"
-    context_object_name = "post"
-    ordering = ["-created_at"]
-    paginate_by = 5
+class PostDetailView(TemplateView):
+    template_name = "post_detail.html"
+
+
+# class PostListView(ListView):
+#     model = Post
+#     template_name = "posts/post_list.html"
+#     context_object_name = "posts"
+#     ordering = ["-created_at"]
+#     paginate_by = 5
+
+
+# class PostDetailView(DetailView):
+#     model = Post
+#     template_name = "posts/post_detail.html"
+#     context_object_name = "post"
+#     ordering = ["-created_at"]
+#     paginate_by = 5
