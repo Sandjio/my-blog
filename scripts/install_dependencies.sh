02b2ff6a672d5256640896cafc5467fcab8b77ba
sandjio <emmasandjio@gmail.com>
2025-03-08 17:37:03 +0100

    update scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/my-blog
-python3 -m venv venv
-source venv/bin/activate
+# python3 -m venv venv
+source /home/ubuntu/my-blog/venv/bin/activate
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
